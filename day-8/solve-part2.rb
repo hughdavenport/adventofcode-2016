c4b77b65c038d2b6fed09968c74ce010fdb08dbd
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-12-30 07:42:02 +1300

    Solved day 8 part 2

---
+++ day-8/solve-part2.rb
@@ -0,0 +1,32 @@
+instructions = File.readlines(ARGV[0]).map(&:chomp)
+
+screen = [[nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
+          [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
+          [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
+          [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
+          [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
+          [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]]
+
+instructions.each do |instruction|
+  if instruction[0..4] == "rect "
+    x, y = instruction[5..-1].split("x").map(&:to_i)
+    y.times do |y|
+      x.times do |x|
+        screen[y][x] = true
+      end
+    end
+  elsif instruction[7..10] == "row "
+    row, shift = instruction.split("=")[1].split(" by ").map(&:to_i)
+    screen[row] = screen[row].slice(-(shift)..-1) + screen[row].slice(0..-(shift+1))
+  elsif instruction[7..13] == "column "
+    column, shift = instruction.split("=")[1].split(" by ").map(&:to_i)
+    screen = screen.transpose
+    screen[column] = screen[column].slice(-(shift)..-1) + screen[column].slice(0..-(shift+1))
+    screen = screen.transpose
+  end
+end
+
+# Flag trying to work out what the letter is, my eye does a better job for this amount
+screen.each do |row|
+  puts row.map { |cell| cell ? "*" : " " }.join
+end
